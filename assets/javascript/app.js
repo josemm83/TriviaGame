e1712a0aa22c4aa1a348c2d9b07a9261141db570
josemm83 <josemmancha@gmail.com>
2020-01-11 14:11:20 -0600

    updating homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,107 +1,124 @@
+var questions = [];
 var rightAnswer = 0;
 var wrongAnswer = 0;
+var timeOut = 0; 
+var userInput;
 var counter = 0;
 var timer = 30;
-var timer2 = 5;
+var running = false;
 var selectedQuestion;
+var choices;
 var intervalID;
-
-var questions = {
-        ques1: {
-        q: "What is the Texas state plant?",
-        a1: "Sunflower",
-        a2: "Bluebonnet",
-        a3: "Prickly pear cactus",
-        a4: "Daisy",
-        pic: "../images/prickly-pear-cactus.webp"
-    },
-    
-    ques2: {
-        q: "How many championships did Michael Jordan win with the Chicago Bulls?",
-        a1: "7",
-        a2: "4",
-        a3: "3",
-        a4: "6",
-        pic: "../images/jordan.jpg"
-    },
-    
-    ques3: { 
-        q: "How many soccer players should a team have on the field at the start of a match?",
-        a1: "11",
-        a2: "10",
-        a3: "15",
-        a4: "12",
-        pic: "..images/soccer.png"
-    },
-    
-    ques4: {
-        q: "What year was the very first model of the Iphone released?",
-        a1: "2006",
-        a2: "2005",
-        a3: "2004",
-        a4: "2007",
-        pic: "../images/iphone.jpg"
-    },
-        
-    ques5 : {
-        q: "Which planet is the hottest in this solar system?",
-        a1: "Mars",
-        a2: "Sun",
-        a3: "Venus",
-        a4: "Mercury",
-        pic: "../images/venus.jfif"
-    },
-        
-    ques6: {
-        q: "Which natural disaster is measured by a Richter scale?",
-        a1: "Tornados",
-        a2: "Earthquake",
-        a3: "Global Warming",
-        a4: "Hurricane",
-        pic: "../image/earthquake.jpg"
-    },
-    
-    ques7: {
-        q: "Which planet has the most gravity in this solar system?",
-        a1: "Pluto",
-        a2: "Sun",
-        a3: "Uranus",
-        a4: "Jupiter",
-        pic: "../images/jupiter.jfif"
-    },
-        
-    ques8: {
-        q: "Which country produces the most coffee in the world?",
-        a1: "Starbucks",
-        a2: "Paris",
-        a3: "North America",
-        a4: "Brazil",
-        pic: "../images/brazil.jpg"
-    }
-};
+var limit = 5;
+var picture;
 
 function randomQuestion(){
-
+    if (questions.length === 0){
+        exitGame();
+    }
+    else{
+        var random = Math.floor(Math.random() * questions.length);
+        selectedQuestion = questions[random];
+        questions.splice(random, 1);
+        // console.log(selectedQuestion);
+        $("#randomQuestion").html("<h2>" + selectedQuestion.q + "</h2>");
+        for (var i = 0; i < selectedQuestion.choice.length; i++){
+            var multipleChoice = $("<div>");
+            multipleChoice.addClass("answerPick");
+            multipleChoice.html(selectedQuestion.choice[i]);
+            
+            $("#choices").append(multipleChoice);
+        }
+    }
 }
 
 function decrement() {
+    clearInterval(timer);
+    $("#timer").html("<h3>Time Remaining: " + timer + " Seconds</h3>");
     timer --;
-    $("#timer").html("<h2>" + timer + "</h2>");
     if (timer === 0) {
-      stop();
-      alert("Time Up!");
+      clearInterval(timer);
+      running = false;
+      timeOut ++;
+    //   alert("Time Up!");
     }
 }
 
-function stop() {
-    clearInterval();
+function exitGame(){
+    $("#results").append("Correct: " + rightAnswer);
+    $("#results").append("Incorrect: " + wrongAnswer);
+    $("#results").append("Time out: " + timeOut);
+    $("#reset").show();
 }
 
 function gameStart() {
-    
+    running = true;
+    randomQuestion();
+
 }
 
-$(document).on("click", "#start", function() {
-    console.log("Made it here!");
+$(document).ready(function () {
+    $("#reset").hide();
+    questions = [
+        { 
+            q: "What is the Texas state plant?",
+            choice: ["Sunflower", "Bluebonnet", "Prickly pear cactus", "Daisy"],
+            answer: 2,
+            pic: "../images/prickly-pear-cactus.webp"
+        },
+        {
+            q: "How many championships did Michael Jordan win with the Chicago Bulls?",
+            choice: ["7", "4", "3", "6"],
+            answer: 3,
+            pic: "../images/jordan.jpg"
+        },
+        { 
+            q: "How many soccer players should a team have on the field at the start of a match?",
+            choice: ["11", "10", "15", "12"],
+            answer: 0,
+            pic: "..images/soccer.png"
+        },
+        {
+            q: "What year was the very first model of the Iphone released?",
+            choice: ["2006", "2005", "2004", "2007"],
+            answer: 3,
+            pic: "../images/iphone.jpg"
+        },    
+        {
+            q: "Which planet is the hottest in this solar system?",
+            choice: ["Mars", "Sun", "Venus", "Mercury"],
+            answer: 2,
+            pic: "../images/venus.jfif"
+        },
+        {
+            q: "Which natural disaster is measured by a Richter scale?",
+            choice: ["Tornados", "Earthquake", "Global Warming", "Hurricane"],
+            answer: 1,
+            pic: "../image/earthquake.jpg"
+        },
+        {
+            q: "Which planet has the most gravity in this solar system?",
+            choice: ["Pluto", "Sun", "Uranus", "Jupiter"],
+            answer: 3,
+            pic: "../images/jupiter.jfif"
+        },
+        {
+            q: "Which country produces the most coffee in the world?",
+            choice: ["Starbucks", "Paris", "North America", "Brazil"],
+            answer: 1,
+            pic: "../images/brazil.jpg"
+        }];
+    // console.log(questions);
+    // console.log(questions.length);
+});
+
+$("#start").on("click", function() {
+    $("#start").hide();
+    $("#reset").hide();
+    setInterval(decrement, 1000);
     gameStart();
+});
+
+$("#reset").on("click", function () {
+
 });
